6fabf20f079351e60644ee1852b59f5f9f90755c
Josiah Bull <josiah.bull7@gmail.com>
2021-09-14 19:51:57 +1200

    test: Implemented basic test framework, and set up a github actions automatic test.

---
+++ src/test/java/com/se206/g11/MainAppTest.java
@@ -0,0 +1,13 @@
+package com.se206.g11;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import org.junit.jupiter.api.Test;
+
+public class MainAppTest {
+
+    // Placeholder test to ensure ci/cd pipeline is working correctly
+    @Test
+    public void it_works() {
+        assertTrue(true);
+    }
+}
