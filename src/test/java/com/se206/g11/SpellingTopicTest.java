2b23f37d617f52fb6e9e8bb8366ef6d179da41b7
Josiah Bull <josiah.bull7@gmail.com>
2021-09-17 15:37:56 +1200

    tests: Created some empty test classes to be populated at a later date.

---
+++ src/test/java/com/se206/g11/SpellingTopicTest.java
@@ -0,0 +1,15 @@
+package com.se206.g11;
+
+import org.junit.jupiter.api.Test;
+
+public class SpellingTopicTest {
+    @Test
+    void testNameTitleCase() {
+
+    }
+
+    @Test
+    void testPath() {
+
+    }
+}
---
+++ src/test/java/com/se206/g11/WordTest.java
@@ -0,0 +1,25 @@
+package com.se206.g11;
+
+import org.junit.jupiter.api.Test;
+
+public class WordTest {
+    @Test
+    void testEnglish() {
+
+    }
+
+    @Test
+    void testIsEqualLazy() {
+
+    }
+
+    @Test
+    void testIsEqualStrict() {
+
+    }
+
+    @Test
+    void testMaori() {
+
+    }
+}
