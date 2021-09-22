5483c27fdead27556ce523f8ad60e4bf236f0bbe
johnchen383 <john.bm.chen@gmail.com>
2021-09-22 13:11:16 +1200

    feat: finished implementing all topic selection

--- src/main/java/com/se206/g11/screens/TopicScreenController.java
+++ src/main/java/com/se206/g11/screens/TopicScreenController.java
@@ -23,6 +23,26 @@ public class TopicScreenController extends ApplicationController implements Init
     private ImageView compassPoints;
     @FXML
     private ImageView colours;
+    @FXML
+    private ImageView daysOfWeek;
+    @FXML
+    private ImageView daysOfWeekLoanWords;
+    @FXML
+    private ImageView engineering;
+    @FXML
+    private ImageView feelings;
+    @FXML
+    private ImageView monthsOfYear;
+    @FXML
+    private ImageView monthsOfYearLoanWords;
+    @FXML
+    private ImageView software;
+    @FXML
+    private ImageView uniLife;
+    @FXML
+    private ImageView weather;
+    @FXML
+    private ImageView work;
 
     @Override
     public void initialize(URL url, ResourceBundle rb) {
@@ -33,6 +53,18 @@ public class TopicScreenController extends ApplicationController implements Init
         }
 
         initiliseSelectableTopic(babies);
+        initiliseSelectableTopic(compassPoints);
+        initiliseSelectableTopic(colours);
+        initiliseSelectableTopic(daysOfWeek);
+        initiliseSelectableTopic(daysOfWeekLoanWords);
+        initiliseSelectableTopic(engineering);
+        initiliseSelectableTopic(feelings);
+        initiliseSelectableTopic(monthsOfYear);
+        initiliseSelectableTopic(monthsOfYearLoanWords);
+        initiliseSelectableTopic(software);
+        initiliseSelectableTopic(uniLife);
+        initiliseSelectableTopic(weather);
+        initiliseSelectableTopic(work);
     }
 
     private void initiliseSelectableTopic(ImageView id){
@@ -41,10 +73,10 @@ public class TopicScreenController extends ApplicationController implements Init
 
             if (TOPICS.contains(topic)){
                 chosenTopic = topic;
-                // System.out.println("Entering Game! with topic: " + topic);
-                MainApp.setRoot("GameScreen", "Kemu Kupu - Let's Play!");
+                System.out.println("Entering Game! with topic: " + topic.getName());
+                // MainApp.setRoot("GameScreen", "Kemu Kupu - Let's Play!");
             } else {
-                System.err.println("Could not initialise listview with id: " + id.toString());
+                System.err.println("Could not select topic with id: " + id.getId());
             }
         });
     }
