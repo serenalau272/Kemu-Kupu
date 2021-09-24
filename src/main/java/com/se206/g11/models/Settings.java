5afce5189d43efb5c838112149361d952457c493
Josiah Bull <josiah.bull7@gmail.com>
2021-09-24 18:37:49 +1200

    feat (settings): fxml and controller setup for settings page.

---
+++ src/main/java/com/se206/g11/models/Settings.java
@@ -0,0 +1,59 @@
+package com.se206.g11.models;
+
+public class Settings {
+    private boolean music = true;
+    private Double speechSpeed = 1.0;
+
+    /**
+     * Initalize settings with default values
+     */
+    public Settings() {
+        
+    }
+
+    /**
+     * Serialize the settings from a file
+     * @param p the path to the file
+     */
+    public void load(String p) {
+        //TODO
+    }
+
+    /**
+     * Save the settings to the path
+     * @param p the path to save to
+     */
+    public void save(String p) {
+        //TODO
+    }
+
+    /**
+     * @return whether music is enabled
+     */
+    public boolean getMusic() {
+        return this.music;
+    }
+
+    /**
+     * @return the speed speed
+     */
+    public Double getSpeechSpeed() {
+        return this.speechSpeed;
+    }
+
+    /**
+     * Set whether music is available or not
+     * @param e
+     */
+    public void setMusic(boolean e) {
+        this.music = e;
+    }
+    
+    /**
+     * Set the speaking speed of the tts implementation (festival)
+     * @param s
+     */
+    public void setSpeechSpeed(Double s) {
+        this.speechSpeed = s;
+    }
+}
---
+++ src/main/java/com/se206/g11/screens/SettingsScreenController.java
@@ -0,0 +1,18 @@
+package com.se206.g11.screens;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import com.se206.g11.ApplicationController;
+
+import javafx.fxml.Initializable;
+
+public class SettingsScreenController extends ApplicationController implements Initializable {
+
+    
+
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        
+        // TODO
+    }    
+}
