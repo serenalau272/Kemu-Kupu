caa83217d9a9df654efba7b4d91619b2f385ea22
Josiah Bull <josiah.bull7@gmail.com>
2021-10-09 12:00:51 +1300

    feat: users endpoints implemented. - Account creation   - Includes password hashing - Account deletion - Logging in

--- api/src/main.rs
+++ api/src/main.rs
@@ -4,10 +4,20 @@ use rocket::fs::NamedFile;
 
 #[macro_use]
 extern crate rocket;
+#[macro_use]
+extern crate diesel;
 
-mod database;
+mod common;
 mod error;
-mod structs;
+mod models;
+mod schema;
+
+use diesel::prelude::*;
+use rocket::http::Status;
+use rocket::serde::json::Json;
+/// Database connection
+#[rocket_sync_db_pools::database("postgres_database")]
+struct UsersDbConn(diesel::PgConnection);
 
 /// Thinking about endpoints
 /// GET api/v1/student/{id}, request information about a user, must contain AUTH header with their token.
@@ -16,37 +26,270 @@ mod structs;
 /// GET api/v1/highscores, to request a list of all the high scores
 /// POST /api/va/highscore, to add a new score - must contain information about the user
 
+/// TODO General todos
+/// implement JWT system
+/// Move common DB requests (such as looking up a user) into a framework under common.rs to avoid duplicate code.
+
 /// Return information about a student
-#[get("/api/v1/student/<id>")]
-async fn get_student(id: String) {
-    
+#[get("/api/v1/student/<search_id>", format = "application/json")]
+async fn get_student(search_id: i32, conn: UsersDbConn) -> (Status, String) {
+    //TODO authentication here
+
+    //Load the item from the db, if it exists
+    use crate::schema::users::dsl::*;
+    let r: Option<crate::models::User> = conn
+        .run(move |c| {
+            let r = users
+                .filter(id.eq(search_id))
+                .limit(1)
+                .load::<crate::models::User>(c);
+            if let Ok(mut v) = r {
+                if v.is_empty() {
+                    return None;
+                }
+                return Some(v.remove(0));
+            }
+            return None;
+        })
+        .await;
+
+    // Format and return
+    if let Some(user) = r {
+        let json_response = models::Response { data: user }.to_json();
+        if let Err(e) = json_response {
+            return (Status::InternalServerError, e.to_string());
+        }
+        return (
+            Status::Ok,
+            json_response.unwrap(), //Safety: Checked above with if let Err();
+        );
+    }
+    (
+        Status::BadRequest,
+        models::Response {
+            data: "User Not Found",
+        }
+        .to_json()
+        .unwrap(),
+    )
 }
 
 /// Attempt to login as a student
-#[post("/api/v1/student/login")]
-async fn login_student() {
-    
+#[post(
+    "/api/v1/student/login",
+    data = "<login_information>",
+    format = "application/json"
+)]
+async fn login_student(
+    conn: UsersDbConn,
+    login_information: Json<models::UserCredentials>,
+) -> (Status, String) {
+    let login_information = login_information.into_inner();
+    //Check if the user exists in the db
+    use crate::schema::users::dsl::*;
+    let name = login_information.usr.clone();
+    let r: Option<crate::models::User> = conn
+        .run(move |c| {
+            let r = users
+                .filter(usr.eq(name))
+                .limit(1)
+                .load::<crate::models::User>(c);
+            if let Ok(mut v) = r {
+                if v.is_empty() {
+                    return None;
+                }
+                return Some(v.remove(0));
+            }
+            return None;
+        })
+        .await;
+
+    if r.is_none() {
+        return (
+            Status::BadRequest,
+            models::Response {
+                data: "Incorrect Password or Username",
+            }
+            .to_json()
+            .unwrap(),
+        );
+    }
+
+    //Check that their password hash matches
+    let hash_valid = match common::compare_hashed_strings(login_information.pwd, r.unwrap().pwd) {
+        Ok(h) => h,
+        Err(e) => {
+            return (
+                Status::InternalServerError,
+                models::Response {
+                    data: format!("Failed to compare hashes {}", e.to_string()),
+                }
+                .to_json()
+                .unwrap(),
+            )
+        }
+    };
+
+    if ! hash_valid {
+        return (
+            Status::BadRequest,
+            models::Response {
+                data: "Incorrect Password or Username"
+            }
+            .to_json()
+            .unwrap(),
+        );
+    }
+
+    //TODO return useful login token here, for now just tell them they're cool.
+    return (
+        Status::Created,
+        models::Response { data: "Logged In" }.to_json().unwrap(),
+    );
 }
 
 /// Create a new student
-#[post("/api/v1/student")]
-async fn create_student() {
+#[post("/api/v1/student", data = "<new_user>", format = "application/json")]
+async fn create_student(
+    conn: UsersDbConn,
+    new_user: Json<models::UserCredentials>,
+) -> (Status, String) {
+    //Check their password meets minimum requirements
+    let new_user = new_user.into_inner();
+    if new_user.pwd.len() < 8 {
+        return (
+            Status::BadRequest,
+            models::Response {
+                data: "Password Too Short",
+            }
+            .to_json()
+            .unwrap(),
+        );
+    }
 
+    //Check that the username isnt't taken
+    use crate::schema::users::dsl::*;
+    let name = new_user.usr.clone();
+    let r: Option<crate::models::User> = conn
+        .run(move |c| {
+            let r = users
+                .filter(usr.eq(name))
+                .limit(1)
+                .load::<crate::models::User>(c);
+            if let Ok(mut v) = r {
+                if v.is_empty() {
+                    return None;
+                }
+                return Some(v.remove(0));
+            }
+            return None;
+        })
+        .await;
+
+    if r.is_some() {
+        return (
+            Status::BadRequest,
+            models::Response {
+                data: "Username Taken",
+            }
+            .to_json()
+            .unwrap(),
+        );
+    }
+
+    //Hash password
+    let hashed_password = match common::hash_string_with_salt(new_user.pwd) {
+        Ok(p) => p,
+        Err(e) => {
+            return (
+                Status::InternalServerError,
+                models::Response {
+                    data: format!("Unable to hash password {}", e.to_string()),
+                }
+                .to_json()
+                .unwrap(),
+            )
+        }
+    };
+
+    let new_user = models::UserCredentials {
+        usr: new_user.usr,
+        pwd: hashed_password,
+    };
+
+    //Save account in db
+    use schema::users;
+    let r: Result<models::User, diesel::result::Error> = conn
+        .run(move |c| {
+            diesel::insert_into(users::table)
+                .values(new_user)
+                .get_result(c)
+        })
+        .await;
+
+    if let Err(e) = r {
+        return (
+            Status::InternalServerError,
+            models::Response {
+                data: format!("Failed to insert into server {}", e.to_string()),
+            }
+            .to_json()
+            .unwrap(),
+        );
+    }
+
+    //TODO Return valid token
+    return (
+        Status::Created,
+        models::Response {
+            data: "Account Created",
+        }
+        .to_json()
+        .unwrap(),
+    );
 }
 
-#[delete("/api/v1/student/<id>")]
-async fn delete_student(id: String) {
+#[delete("/api/v1/student/<user_id>", format = "application/json")]
+async fn delete_student(user_id: i32, conn: UsersDbConn) -> (Status, String) {
+    //TODO: Authentication
 
+    //Delete student from db
+    use crate::schema::users::dsl::*;
+    let r: Result<crate::models::User, diesel::result::Error> = conn.run(move |c| {
+        diesel::delete(users.filter(id.eq(user_id)))
+            .get_result(c)
+    }).await;
+
+    if let Err(e) = r {
+        return (
+            Status::InternalServerError,
+            models::Response {
+                data: format!("Unable to delete user due to error {}", e.to_string()),
+            }
+            .to_json()
+            .unwrap(),
+        );
+    }
+
+    return (
+        Status::Ok,
+        models::Response {
+            data: format!("Account {} deleted", r.unwrap().usr),
+        }
+        .to_json()
+        .unwrap(),
+    )
 }
 
 #[get("/api/v1/highscores")]
-async fn get_highscores() {
-
+async fn get_highscores(conn: UsersDbConn) {
+    //TODO allow params here for pagination
 }
 
 #[post("/api/v1/highscores")]
-async fn add_score() {
-    
+async fn add_score(conn: UsersDbConn) {
+    //TODO authentication
+    //Add score to db
 }
 
 /// Serve docs about the api
@@ -58,7 +301,7 @@ fn docs() {
 /// Returns the current health status of the database
 #[get("/health")]
 fn health() -> String {
-    "Hello, World!".into()
+    "Good".into()
 }
 
 /// Handle the serving of any static resources for various pages
@@ -76,15 +319,20 @@ async fn not_found() -> Option<NamedFile> {
 
 #[launch]
 fn rocket() -> _ {
-    rocket::build().register("/", catchers![not_found]).mount(
-        "/",
-        routes![
-            get_student,
-            login_student,
-            create_student,
-            delete_student,
-            get_highscores,
-            add_score,
-        ],
-    )
+    rocket::build()
+        .register("/", catchers![not_found])
+        .mount(
+            "/",
+            routes![
+                get_student,
+                login_student,
+                create_student,
+                delete_student,
+                get_highscores,
+                add_score,
+                website_resource,
+                health,
+            ],
+        )
+        .attach(UsersDbConn::fairing())
 }
