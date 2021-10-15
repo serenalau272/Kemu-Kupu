ef14a6a1be4d3be513ac13321b8a8a3da7d5027d
Josiah Bull <josiah.bull7@gmail.com>
2021-10-16 12:14:30 +1300

    feat: Added mock requests for the user class to the api.

--- src/main/java/com/models/User.java
+++ src/main/java/com/models/User.java
@@ -2,10 +2,13 @@ package com.models;
 
 import java.util.Map;
 import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
 
 import com.enums.Avatar;
 
 import java.util.AbstractMap;
+import java.util.ArrayList;
 
 /**
  * This class handles the storing and retreiving of the current user.
@@ -13,7 +16,7 @@ import java.util.AbstractMap;
 public class User {
     private String name;
     private Avatar selectedAvatar;
-    private Map<Avatar, Boolean> boughtAvatars = new HashMap<Avatar, Boolean>();
+    private HashSet<Avatar> boughtAvatars = new HashSet<>();
     private Integer stars;
     private Integer highScore;
 
@@ -30,7 +33,7 @@ public class User {
         new AbstractMap.SimpleEntry<Avatar, Integer>(Avatar.NASSER, 100)
     );
 
-    User(){
+    User() {
         //guest
         this.name = "";
         this.selectedAvatar = Avatar.DEFAULT;
@@ -39,30 +42,84 @@ public class User {
         this.highScore = null;
     }
 
-    public void login(String name, String password){
+    /**
+     * Login a user to the api
+     * @param name the username of the account
+     * @param password the password of the account
+     * @throws Exception if unable to complete the request
+     */
+    public void login(String name, String password) throws Exception {
         //verify and retrieve all fields
-
     }
 
-    public void signup(String name, String password){
+    /**
+     * Create a new user with the api, and log them in automatically.
+     * @param name the username
+     * @param password the password to signup with
+     * @throws Exception if unable to complete the request
+     */
+    public void signup(String name, String password) throws Exception {
         //verify u can create this user
         this.name = name;
         this.stars = 0;
         this.highScore = 0;
-        this.boughtAvatars = Map.ofEntries(
-            new AbstractMap.SimpleEntry<Avatar, Boolean>(Avatar.DEFAULT, true),
-            new AbstractMap.SimpleEntry<Avatar, Boolean>(Avatar.NASSER, false),
-            new AbstractMap.SimpleEntry<Avatar, Boolean>(Avatar.ALIEN, false),
-            new AbstractMap.SimpleEntry<Avatar, Boolean>(Avatar.CHEF, false),
-            new AbstractMap.SimpleEntry<Avatar, Boolean>(Avatar.FAIRY, false),
-            new AbstractMap.SimpleEntry<Avatar, Boolean>(Avatar.NINJA, false),
-            new AbstractMap.SimpleEntry<Avatar, Boolean>(Avatar.PRINCESS, false),
-            new AbstractMap.SimpleEntry<Avatar, Boolean>(Avatar.SAILOR, false),
-            new AbstractMap.SimpleEntry<Avatar, Boolean>(Avatar.TUXEDO, false),
-            new AbstractMap.SimpleEntry<Avatar, Boolean>(Avatar.WIZARD, false)
-        );
+        this.boughtAvatars = new HashSet<Avatar>() {
+            {
+                add(Avatar.WIZARD);
+            }
+        };
 
         //post all fields
+    }
+
+    /**
+     * Save a new score for this user, should be called at the end of the quiz.
+     * @param score the score to be saved
+     * @throws Exception throws if unable to complete the request
+     */
+    public void addScore(int score) throws Exception {
+
+        //Unable to add score
+    }
+
+    /**
+     * Collect this users scores from the api
+     * @return a list of this users scores from the api
+     * @throws Exception if unable to complete request
+     */
+    public List<Integer> getScores() throws Exception {
+        List<Integer> arr = new ArrayList<Integer>() {
+            {
+                add(15);
+                add(20);
+                add(60);
+                add(75);
+                add(90);
+            }
+        };
+        return arr;
+    }
+
+    /**
+     * Request this users list of unlocked costumes from the api.
+     * @return a hashset containing all of the users bought costumes
+     * @throws Exception if unable to complete the request.
+     */
+    public HashSet<Avatar> getCostumes() throws Exception {
+        return new HashSet<Avatar>() {
+            {
+                add(Avatar.PRINCESS);
+                add(Avatar.WIZARD);
+                add(Avatar.DEFAULT);
+            }
+        };
+    }
+
+    /**
+     * Request the api to unlock a costume for this user
+     * @throws Exception if unable to complete the request
+     */
+    public void unlockCostume() throws Exception {
 
     }
 }
