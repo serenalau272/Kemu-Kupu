a5198ed68b4d738d09c7c3aaa7b12854286ed7ce
johnchen383 <john.bm.chen@gmail.com>
2021-10-25 14:05:34 +1300

    refactor: OscillatingComponent.java

--- src/main/java/com/components/animations/OscillatingComponent.java
+++ src/main/java/com/components/animations/OscillatingComponent.java
@@ -2,27 +2,28 @@ package com.components.animations;
 
 import javafx.animation.Animation;
 import javafx.animation.Transition;
-import javafx.fxml.FXML;
 import javafx.scene.image.ImageView;
 import javafx.util.Duration;
 
+/**
+ * OscillatingComponent is used to oscillate both the play button and avatar along the vertical axis
+ */
 public class OscillatingComponent {
-    @FXML
-    private ImageView image;
     private Animation anim;
     private double initialY;
     private final int amplitude = 10;
 
     public OscillatingComponent(ImageView image) {
-        this.image = image;
         this.initialY = image.getTranslateY();
 
+        //create transition animation with 2s indefinite cycles
         anim = new Transition() {
             {
                 setCycleDuration(Duration.millis(2000));
                 setCycleCount(Animation.INDEFINITE);
             }
 
+            //using a sinusoid function we alter the y position of the ImageView
             protected void interpolate(double frac) {
                 image.setTranslateY(initialY + (Math.sin(frac * Math.PI * 2) * amplitude));
             }
@@ -30,6 +31,10 @@ public class OscillatingComponent {
         };
     }
 
+    /**
+     * return the animator
+     * @return
+     */
     public Animation getAnimator() {
         return anim;
     }
