8ca3255dee84c218873ba24f16ca264ccc1c90de
johnchen383 <john.bm.chen@gmail.com>
2021-10-25 14:01:06 +1300

    refactor: GlobalTimer.java

--- src/main/java/com/components/animations/GlobalTimer.java
+++ src/main/java/com/components/animations/GlobalTimer.java
@@ -2,6 +2,10 @@ package com.components.animations;
 
 import javafx.animation.AnimationTimer;
 
+/**
+ * GlobalTimer is used to keep track of how long a user has to wait globally
+ * before they have to spin the wheel
+ */
 public class GlobalTimer {
     private int timeSeconds;
     private MyTimer timer;
@@ -14,23 +18,40 @@ public class GlobalTimer {
         timer.start();
     }
 
+    /**
+     * Stop timer
+     */
     public void stop() {
         timer.stop();
     }
 
+    /**
+     * Restart timer
+     */
     public void restart() {
         timeSeconds = duration;
         timer.start();
     }
 
+    /**
+     * Get current duration of timer
+     * 
+     * @return
+     */
     public int getDuration() {
         return timeSeconds;
     }
 
+    /**
+     * Timer class for global timer
+     */
     public class MyTimer extends AnimationTimer {
         private long lastUpdate = 0;
-        private long durationTick = 1_000_000_000;
+        private long durationTick = 1_000_000_000; // every second
 
+        /**
+         * Call doHandle() every second
+         */
         @Override
         public void handle(long now) {
             if (now - lastUpdate >= durationTick) {
@@ -39,6 +60,9 @@ public class GlobalTimer {
             }
         }
 
+        /**
+         * action function to decrement time in seconds by 1 and stop if reaching 0
+         */
         private void doHandle() {
             if (timeSeconds <= 1) {
                 stop();
