010a2075debcfab7e0342a9a88ef1e14531d2a75
johnchen383 <john.bm.chen@gmail.com>
2021-10-25 14:09:09 +1300

    refactor: SlideComponentHorizontal.java

--- src/main/java/com/components/animations/SlideComponentHorizontal.java
+++ src/main/java/com/components/animations/SlideComponentHorizontal.java
@@ -5,6 +5,9 @@ import javafx.animation.Transition;
 import javafx.scene.image.ImageView;
 import javafx.util.Duration;
 
+/**
+ * SlideComponentHorizontal is used to animate the secondary buttons from the menu offscreen before transition
+ */
 public class SlideComponentHorizontal {
     private ImageView image;
     private Animation anim;
@@ -15,10 +18,13 @@ public class SlideComponentHorizontal {
     public SlideComponentHorizontal(ImageView image, Duration duration, double delta) {
         this.image = image;
         this.initialX = image.getTranslateX();
-        this.delta = delta;
+        this.delta = delta;     //delta is the total offset of the image's horizontal position
         this.duration = duration;
     }
 
+    /**
+     * Construct the animator for a single cycle of given duration
+     */
     private void constructAnimator() {
         anim = new Transition() {
             {
@@ -26,12 +32,17 @@ public class SlideComponentHorizontal {
                 setCycleCount(1);
             }
 
+            //move the image along the horizontal axis by a given fraction of delta
             protected void interpolate(double frac) {
                 image.setTranslateX(initialX + frac * delta);
             }
         };
     }
 
+    /**
+     * construct the animator and return this
+     * @return
+     */
     public Animation getAnimator() {
         constructAnimator();
         return anim;
